d066e2883d06c6e58e4a050308a21844e4c4c758
Lookandme <kgg9386@gmail.com>
2024-11-15 20:15:52 +0900

    [Add] 플레이어 스크립트 추가

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [field: Header("Animation")]
+    [field: SerializeField] AnimationData animationData;
+
+    public Animator animator {  get; private set; }
+    private void Awake()
+    {
+        animationData.Initialize();
+        animator = GetComponentInChildren<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
