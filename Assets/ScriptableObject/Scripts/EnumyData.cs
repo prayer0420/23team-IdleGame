f1fc72b0882a74d236d354b22e4e58807109b470
Lookandme <kgg9386@gmail.com>
2024-11-21 09:53:34 +0900

    [Update] 애너미 데이터 프로퍼티 수정

--- Assets/ScriptableObject/Scripts/EnumyData.cs
+++ Assets/ScriptableObject/Scripts/EnumyData.cs
@@ -14,8 +14,8 @@ public class EnumyData
     [field: SerializeField][field: Range(0f, 300f)] public float MaxHealth { get; set; } 
     [field: SerializeField][field: Range(0f, 10f)] public float Speed { get; set; }
 
-    [field: SerializeField][field: Range(0f, 10f)] public float poisonDuration = 2f;  // 독 지속 시간 (초)
-    [field: SerializeField][field: Range(0f, 10f)] public float poisonInterval = 0.3f;  // 독 데미지가 주어지는 간격 (초)
-    [field: SerializeField][field: Range(0f, 10f)] public int poisonDamage = 5;  // 독으로 입는 데미지
+    [field: SerializeField][field: Range(0f, 10f)] public float poisonDuration { get; set; } = 2f;  // 독 지속 시간 (초)
+    [field: SerializeField][field: Range(0f, 10f)] public float poisonInterval { get; set; } = 0.3f;  // 독 데미지가 주어지는 간격 (초)
+    [field: SerializeField][field: Range(0f, 10f)] public int poisonDamage { get; set; } = 5;  // 독으로 입는 데미지
     
 }
